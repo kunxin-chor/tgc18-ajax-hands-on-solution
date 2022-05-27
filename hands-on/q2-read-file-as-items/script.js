9c7a83d79e814b7794602662e1f2148f256e9c2f
kunxin-chor <chorkunxin@yahoo.com>
2022-05-27 02:53:43 +0000

    Added solution 2

---
+++ hands-on/q2-read-file-as-items/script.js
@@ -0,0 +1,40 @@
+// document.querySelector('#btnLoad')
+//     .addEventListener('click', async function(){
+//         let response = await axios.get('items.txt');4
+//         let fruits = response.data.split(',');
+//         let fruitList = document.querySelector('#fruits');
+//         for (let f of fruits) {
+//             fruitList.innerHTML = fruitList.innerHTML + `<li>${f}</li>` 
+//         }
+//     })
+
+// document.querySelector("#btnLoad")
+//     .addEventListener('click', async function(){
+        
+//       let response  = await axios.get('items.txt');
+//       let fruits = response.data.split(',');
+//       // we find the DOM with #fruits
+//       // and cache it into a variable
+//       let fruitList = document.querySelector("#fruits");
+//       for (let f of fruits) {
+//           let liElement = document.createElement('li');
+//           liElement.innerHTML = f;
+//           fruitList.appendChild(liElement);
+//       } 
+//     })
+
+document.querySelector("#btnLoad").addEventListener('click', async function(){
+    let response  = await axios.get('items.txt');
+    let fruits = response.data.split(',');
+      // we find the DOM with #fruits
+      // and cache it into a variable
+      let fruitList = document.querySelector("#fruits");
+      let elements = fruits.map(function(fruit){
+          let liElement = document.createElement('li');
+          liElement.innerHTML = fruit;
+          return liElement;
+      })
+      for(let e of elements) {
+          fruitList.append(liElement);
+      }
+})
